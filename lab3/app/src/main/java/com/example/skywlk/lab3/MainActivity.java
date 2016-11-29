9df8de05166fc9911692ae20f09a91bc77f47565
lodak <lodak00@fesb.hr>
2016-11-29 14:39:39 +0100

    converter

--- lab3/app/src/main/java/com/example/skywlk/lab3/MainActivity.java
+++ lab3/app/src/main/java/com/example/skywlk/lab3/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.skywlk.lab3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,6 +19,13 @@ public class MainActivity extends AppCompatActivity {
         DataStorage.fillData(20);
         myListView.setAdapter(new myAdapter(getApplicationContext()));
 
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Integer dataItem = DataStorage.listViewData.get(i);
+                Toast.makeText(getApplicationContext(), String.valueOf(dataItem * dataItem * dataItem), 2000).show();
+            }
+        });
 
     }
 }
